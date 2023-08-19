72cd9eff46f1e98874ba8b0fbb8a1da4aefeb80e
AndrewMc8 <78245384+AndrewMc8@users.noreply.github.com>
2023-08-19 12:52:43 -0600

    Basic Player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] CharacterController2D controller;
+    [SerializeField] Animator animator;
     [SerializeField] float movementSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Vector2 direction = Vector2.zero;
+    bool jump = false;
+    bool crouch = false;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        direction.x = Input.GetAxis("Horizontal") * movementSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(direction.x));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetBool("Jumping", true);
+            jump = true;
+        }
+
+        controller.Move(direction.x, false, jump);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(direction.x * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    public void OnLand()
+    {
+        animator.SetBool("Jumping", false);
     }
 }
