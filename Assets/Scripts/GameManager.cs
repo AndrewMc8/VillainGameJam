dd0cdc20133116d3ba47fecfd70e369249280c57
KBear100 <kgamett@student.neumont.edu>
2023-08-20 16:12:06 -0600

    TileScreen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     [Header("UI")]
-    [SerializeField] TMP_Text numLeft;
+    [SerializeField] TMP_Text numLeftTxt;
+    [SerializeField] GameObject tileScreen;
+    [SerializeField] GameObject title;
+    [SerializeField] GameObject startBtn;
+    [SerializeField] GameObject exitBtn;
+    [SerializeField] TMP_Text[] evilTxt;
+    [SerializeField] float delay = 3;
 
-    [SerializeField] float muderNum;
+    float timer;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
+        Button sBtn = startBtn.GetComponent<Button>();
+        Button qBtn = exitBtn.GetComponent<Button>();
+        Button titleBtn = tileScreen.GetComponent<Button>();
+        sBtn.onClick.AddListener(StartOnClick);
+        qBtn.onClick.AddListener(ExitOnClick);
+
+        timer = delay;
+    }
+
+    public void Update()
+    { 
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetNumLeft(float numLeft)
     {
-        
+        numLeftTxt.text = numLeft + ": Remain";
+    }
+
+    public void SetTileScreen(bool show)
+    {
+        tileScreen.SetActive(show);
+    }
+
+    public void StartOnClick()
+    {
+        SetTileScreen(false);
+    }
+
+    public void ExitOnClick()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
     }
 }
