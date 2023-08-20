de00902839a24e5b55e9a280c5bdc9e6d49ccf62
KBear100 <kgamett@student.neumont.edu>
2023-08-20 14:04:13 -0600

    Start of UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] TMP_Text numLeft;
+
+    [SerializeField] float muderNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
